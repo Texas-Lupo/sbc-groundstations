e9f381ea7162a35b45bc15ca2e0751ee8b340b40
Texas-Lupo <vulpisfoglia.anon@gmail.com>
2026-07-14 12:31:55 +0300

    Create fluke.mk

---
+++ package/fluke/fluke.mk
@@ -0,0 +1,23 @@
+###############################################################################
+#
+# Fluke
+#
+###############################################################################
+
+FLUKE_VERSION = 805d56080ba0c93f56e70964859267b4858c3a93
+FLUKE_SITE = https://github.com/Texas-Lupo/fluke.git
+FLUKE_SITE_METHOD = git
+FLUKE_INSTALL_TARGET = YES
+
+define FLUKE_INSTALL_TARGET_CMDS
+
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/ground_telemetry.py
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(ADAPTIVE_LINK_PKGDIR)/files/S98fluke
+
+	mkdir -p $(TARGET_DIR)/etc/default
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/default $(ADAPTIVE_LINK_PKGDIR)/files/fluke
+
+endef
+
+
+$(eval $(generic-package))
