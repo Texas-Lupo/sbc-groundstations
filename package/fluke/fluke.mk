4df682f9d01e48435adad38712b5c263327b50f8
Texas-Lupo <vulpisfoglia.anon@gmail.com>
2026-07-15 12:58:39 +0300

    Update fluke.mk

--- package/fluke/fluke.mk
+++ package/fluke/fluke.mk
@@ -12,10 +12,10 @@ FLUKE_INSTALL_TARGET = YES
 define FLUKE_INSTALL_TARGET_CMDS
 
 	$(INSTALL) -m 755 -t $(TARGET_DIR)/usr/bin $(@D)/ground_telemetry.py
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(ADAPTIVE_LINK_PKGDIR)/files/S98fluke
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/init.d $(FLUKE_PKGDIR)/files/S98fluke
 
 	mkdir -p $(TARGET_DIR)/etc/default
-	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/default $(ADAPTIVE_LINK_PKGDIR)/files/fluke
+	$(INSTALL) -m 755 -t $(TARGET_DIR)/etc/default $(FLUKE_PKGDIR)/files/fluke
 
 endef
 
